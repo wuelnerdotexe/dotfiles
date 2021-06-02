3c945d248d408138352c9dae8e3abb8d5f16498e
Wuelner Ofdely Martinez Domingo <wuelnermartinez@outlook.com>
2021-06-02 03:20:15 -0500

    Se agregan todas mis configs y mis temas

---
+++ configs/git-bash/.bashrc
@@ -0,0 +1,1 @@
+eval "$(oh-my-posh --init --shell bash --config ~/scoop/apps/oh-my-posh/current/themes/powerline.omp.json)"
---
+++ configs/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+oh-my-posh --init --shell pwsh --config ~/scoop/apps/oh-my-posh/current/themes/powerline.omp.json | Invoke-Expression
+winfetch
