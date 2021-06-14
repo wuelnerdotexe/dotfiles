465ae14e85867b73d0d667b3bdba71b21fcf7b29
Wuelner Ofdely Martinez Domingo <wuelnermartinez@outlook.com>
2021-06-14 18:03:51 -0500

    Se elimina firefox dev de la instalación de apps

--- scripts/chocolatey/apps.ps1
+++ scripts/chocolatey/apps.ps1
@@ -1,7 +1,7 @@
 # FILE PS1 FOR INSTALL CHOCO APPS IN WINDOWS WITH POWERSHELL.
 
 # Install apps:
-choco install firefox-dev --pre google-backup-and-sync googlechrome spotify tunein-radio vscode wamp-server -y
+choco install google-backup-and-sync googlechrome spotify tunein-radio vscode wamp-server -y
 
 # Install Android Studio:
 choco install androidstudio --execution-timeout=0
