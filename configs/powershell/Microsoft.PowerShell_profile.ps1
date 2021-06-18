72c179065857d3151f5173db900bca2a6b5db124
Wuelner Ofdely Martínez Domingo <wuelner.martinez@outlook.com>
2021-06-18 00:04:48 -0600

    Se instala Oh My Posh

--- configs/powershell/Microsoft.PowerShell_profile.ps1
+++ configs/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,2 @@
-winfetch -memorystyle bar -diskstyle textbar
+winfetch -memorystyle textbar -diskstyle textbar
+oh-my-posh --init --shell pwsh --config ~/scoop/apps/oh-my-posh/current/themes/stelbent.minimal.omp.json | Invoke-Expression
