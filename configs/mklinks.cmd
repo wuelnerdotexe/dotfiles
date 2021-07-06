75f5a0fb562f56be38a2b00ac921d0fd29e83fa4
Wuelner Ofdely Martínez Domingo <wuelner.martinez@outlook.com>
2021-07-06 01:07:03 -0600

    Se eliminan links innecesarios

--- configs/mklinks.cmd
+++ configs/mklinks.cmd
@@ -1,8 +1,5 @@
 Rem BASH SCRIPT FOR MAKE CONFIG FILES SYMBOLIC LINKS IN WINDOWS WITH CMD. 
 
-:: Symbolic link for Powershell $PROFILE file.
-mklink "%USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" "%USERPROFILE%\dotfiles\configs\powershell\Microsoft.PowerShell_profile.ps1"
-
 :: Symbolic link for VSCode settings file.
 mklink "%APPDATA%\Code\User\settings.json" "%USERPROFILE%\dotfiles\configs\vscode\settings.json"
 
