7de1117ed2a80f7ea320c7c039ab0017eee8a143
Wuelner Ofdely Martínez Domingo <wuelner.martinez@outlook.com>
2021-07-18 17:28:30 -0600

    Se mejoran ajustes

--- configs/vimfiles/sources/lightline.vim
+++ configs/vimfiles/sources/lightline.vim
@@ -46,7 +46,7 @@ let g:lightline.active={
     \ }
 
 " Set lightline theme:
-let g:lightline.colorscheme='nord'
+let g:lightline.colorscheme='gruvbox'
 
 " Necessary native settings:
 set wildmenu
--- scripts/scoop/tools.ps1
+++ scripts/scoop/tools.ps1
@@ -1,7 +1,7 @@
 # FILE PS1 FOR INSTALL SCOOP TOOLS ON WINDOWS WITH POWERSHELL.
 
 # Install tools:
-scoop install bitwarden-cli cowsay neovim nodejs ntop
+scoop install bitwarden-cli cowsay nano neovim nodejs ntop
 
 # ---------------------------------------------------------------------------
 # Add scoop extras bucket for caffeine and winfetch:
