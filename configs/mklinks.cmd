9cda52b194125b642e2e891eeebb75c0b2ef3332
Wuelner Ofdely Martínez Domingo <wuelner.martinez@outlook.com>
2021-09-26 20:47:22 -0600

    Se actualizan los links simbólicos

--- configs/mklinks.cmd
+++ configs/mklinks.cmd
@@ -6,12 +6,5 @@
 :: About:    Bash script for make symbolic links for config files in Windows.
 :: ----------------------------------------------------------------------------
 
-:: Symbolic link for VSCode settings file.
-mklink "%APPDATA%\Code\User\settings.json" "%USERPROFILE%\dotfiles\configs\vscode\settings.json"
-
-:: Create dir for Neovim settings.
-mkdir "%LOCALAPPDATA%\nvim\"
-
 :: Symbolic link for Neovim file settings.
-mklink "%LOCALAPPDATA%\nvim\init.vim" "%USERPROFILE%\dotfiles\configs\vim\init.vim"
-mklink "%LOCALAPPDATA%\nvim\coc-settings.json" "%USERPROFILE%\dotfiles\configs\vim\coc-settings.json"
+mklink /J "%LOCALAPPDATA%\nvim\" "%USERPROFILE%\dotfiles\configs\neovim\"
