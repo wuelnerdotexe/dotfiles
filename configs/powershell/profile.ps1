0da7e8674e06844c2628659d482f778c673028c5
Wuelner Ofdely Martínez Domingo <wuelner.martinez@outlook.com>
2021-12-08 22:44:28 -0600

    Se agrega un script para instalar modulos de pwsh

--- configs/powershell/profile.ps1
+++ configs/powershell/profile.ps1
@@ -6,11 +6,14 @@
 # About:    Powershell script for config powershell profile in Windows.
 # -----------------------------------------------------------------------------
 
+# Customize powershell predictions.
+Set-PSReadLineOption -PredictionViewStyle ListView
+
 # Enable powershell predictions.
 Set-PSReadLineOption -PredictionSource History
 
-# Customize powershell predictions.
-Set-PSReadLineOption -PredictionViewStyle ListView
+# Enable terminal icons.
+Import-Module -Name Terminal-Icons
 
 # Init starship prompt.
 Invoke-Expression (&starship init powershell)
---
+++ scripts/powershell/modules.ps1
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------------------------
+# Name:     INSTALL POWERSHELL MODULES.
+# Author:   Wuelner Martínez <wuelner.martinez@outlook.com>
+# URL:      https://github.com/wuelnerdotexe/dotfiles
+# License:  MIT (C) Wuelner Martínez.
+# About:    Powershell script for install powershell modules in Windows.
+# -----------------------------------------------------------------------------
+
+# Install PSReadLine.
+Install-Module -Name PSReadLine -AllowPrerelease -Force
+
+# Install Terminal-Icons.
+Install-Module -Name Terminal-Icons
