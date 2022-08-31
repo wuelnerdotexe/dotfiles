5da1e6474ec269920e0143cf1ad95571d577870d
Wuelner Martínez <wuelner.martinez@outlook.com>
2022-08-31 17:48:28 -0600

    Se realiza una mejora en nvim-cmp

--- neovim/.config/nvim/after/plugin/autocomplete.config.lua
+++ neovim/.config/nvim/after/plugin/autocomplete.config.lua
@@ -18,8 +18,8 @@ cmp.setup({
     vim.fn['vsnip#anonymous'](args.body)
   end },
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-3),
+    ['<C-f>'] = cmp.mapping.scroll_docs(3),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true })
